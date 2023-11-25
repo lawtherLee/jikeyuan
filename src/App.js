03e6fb904d51207f661e845f35f9be8606f4cefa
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-11-25 15:08:59 +0800

    安装路由，创建登录页面配置路由展示

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
+import {BrowserRouter} from "react-router-dom";
+import Route from "react-router-dom/es/Route";
+import Login from "./pages/Login";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route path={'/login'} component={Login}/>
+        </div>
+      </BrowserRouter>
+    
     );
+    
+    
   }
 }
 
