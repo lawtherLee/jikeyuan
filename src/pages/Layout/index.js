e7e39694372073b80671a6954168138d04e8db3d
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-11-26 22:31:54 +0800

    layout数据渲染

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -7,13 +7,42 @@ import {
   LaptopOutlined,
   NotificationOutlined,
   LoginOutlined,
+  HomeOutlined,
+  FormOutlined,
+  CopyOutlined,
 } from "@ant-design/icons";
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
+const menuIcon = {
+  "icon-data": <HomeOutlined />,
+  "icon-content": <CopyOutlined />,
+  "icon-publish": <FormOutlined />,
+};
+
 class LayoutIndex extends Component {
+  state = {
+    menus: [],
+  };
+
+  componentDidMount() {
+    this.getMenus();
+  }
+
+  getMenus() {
+    const menus = [
+      { id: 1, name: "数据概览", icon: "icon-data" },
+      { id: 2, name: "内容管理", icon: "icon-content" },
+      { id: 3, name: "发布文章", icon: "icon-publish" },
+    ];
+    this.setState({
+      menus: menus,
+    });
+  }
+
   render() {
+    const { menus } = this.state;
     return (
       <div className={styles.layout}>
         <Layout>
@@ -30,33 +59,17 @@ class LayoutIndex extends Component {
           <Layout>
             <Sider width={200} className="site-layout-background">
               <Menu
+                theme={"dark"}
                 mode="inline"
                 defaultSelectedKeys={["1"]}
                 defaultOpenKeys={["sub1"]}
                 style={{ height: "100%", borderRight: 0 }}
               >
-                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub3"
-                  icon={<NotificationOutlined />}
-                  title="subnav 3"
-                >
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
+                {menus.map((item) => (
+                  <Menu.Item key={item.id} icon={menuIcon[item.icon]}>
+                    {item.name}
+                  </Menu.Item>
+                ))}
               </Menu>
             </Sider>
             <Layout style={{ padding: "0 24px 24px" }}>
