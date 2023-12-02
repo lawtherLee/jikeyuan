d5baee769916ab962cad606ad1455e08e42b771a
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-12-03 01:34:17 +0800

    文章管理面包屑导航

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import { Breadcrumb } from "antd";
+import styles from "./index.module.scss";
 
 class Article extends Component {
   render() {
-    return <div>文章</div>;
+    return (
+      <div className={styles.article}>
+        <Breadcrumb>
+          <Breadcrumb.Item onClick={() => this.props.history.push("/home")}>
+            首页
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>文章管理</Breadcrumb.Item>
+        </Breadcrumb>
+      </div>
+    );
   }
 }
 
