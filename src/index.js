5fe54f3353dd0e92d5cec7ddce4f1db7caafd702
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-12-09 18:55:58 +0800

    全局配置国际化

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'antd/dist/antd.css';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "antd/dist/antd.css";
+import "./index.css";
+import App from "./App";
+import "moment/locale/zh-cn";
+import locale from "antd/es/locale/zh_CN";
+import { ConfigProvider } from "antd";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App/>
+  <ConfigProvider locale={locale}>
+    <App />
+  </ConfigProvider>,
 );
-
-
--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
-import { Breadcrumb } from "antd";
+import {
+  Breadcrumb,
+  Button,
+  DatePicker,
+  Radio,
+  Divider,
+  Form,
+  Select,
+} from "antd";
 import styles from "./index.module.scss";
+
+const { RangePicker } = DatePicker;
 
 class Article extends Component {
   render() {
@@ -12,6 +22,51 @@ class Article extends Component {
           </Breadcrumb.Item>
           <Breadcrumb.Item>文章管理</Breadcrumb.Item>
         </Breadcrumb>
+
+        <Divider />
+
+        <Form name="basic">
+          <Form.Item name="username" label={"状态"}>
+            <Radio.Group>
+              <Radio value={1}>全部</Radio>
+              <Radio value={2}>草稿</Radio>
+              <Radio value={3}>待审核</Radio>
+              <Radio value={4}>审核通过</Radio>
+              <Radio value={5}>审核失败</Radio>
+            </Radio.Group>{" "}
+          </Form.Item>
+
+          <Form.Item label={"频道"} name={"time"}>
+            <Select
+              defaultValue="lucy"
+              style={{
+                width: 200,
+              }}
+              options={[
+                {
+                  value: "jack",
+                  label: "Jack",
+                },
+                {
+                  value: "lucy",
+                  label: "Lucy",
+                },
+                {
+                  value: "Yiminghe",
+                  label: "yiminghe",
+                },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item label={"日期"}>
+            <RangePicker />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              筛选
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     );
   }
