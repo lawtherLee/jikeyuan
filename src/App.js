cbee3cfc72668d0a831ae374eb173813972d046e
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-12-16 23:27:53 +0800

    解决path为/重定向home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { Switch, Route, Router } from "react-router-dom";
+import { Switch, Route, Router, Redirect } from "react-router-dom";
 import Login from "./pages/Login";
 import Layout from "./pages/Layout";
 import AuthRoute from "./components/AuthRoute";
 import NotFound from "./pages/NotFound";
 import history from "./utils/history";
+import Home from "./pages/Home";
 
 class App extends Component {
   render() {
@@ -14,6 +15,8 @@ class App extends Component {
           <Switch>
             <Route path={"/login"} component={Login} />
             <AuthRoute path={"/home"} component={Layout} />
+
+            <Redirect from={"/"} to={"/home"} />
             <Route component={NotFound} />
           </Switch>
         </div>
