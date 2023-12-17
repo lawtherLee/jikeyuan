e8f68f8253cd97b3d5237ffd6b491ed0a6e0ba62
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-12-17 12:09:54 +0800

    懒加载

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { Switch, Route, Router, Redirect } from "react-router-dom";
-import Login from "./pages/Login";
-import Layout from "./pages/Layout";
+// import Login from "./pages/Login";
+// import Layout from "./pages/Layout";
 import AuthRoute from "./components/AuthRoute";
 import NotFound from "./pages/NotFound";
 import history from "./utils/history";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+
+const Layout = React.lazy(() => import("./pages/Layout"));
+const Login = React.lazy(() => import("./pages/Login"));
 
 class App extends Component {
   render() {
     return (
       <Router history={history}>
         <div>
-          <Switch>
-            <Route path={"/login"} component={Login} />
-            <AuthRoute path={"/home"} component={Layout} />
+          <Suspense fallback={<div>loading</div>}>
+            <Switch>
+              <Route path={"/login"} component={Login} />
+              <AuthRoute path={"/home"} component={Layout} />
 
-            <Redirect from={"/"} to={"/home"} />
-            <Route component={NotFound} />
-          </Switch>
+              <Redirect from={"/"} to={"/home"} />
+              <Route component={NotFound} />
+            </Switch>
+          </Suspense>
         </div>
       </Router>
     );
